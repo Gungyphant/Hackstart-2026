e8f5e2111681465a96be09be9d543789554f2ff8
Gungyphant <gungyphant@gmail.com>
2026-09-19 13:34:35 +0100

    Modified Enemy

--- enemy.py
+++ enemy.py
@@ -1,16 +1,26 @@
 from globals import *
 
 ENEMIES = []  # Global list of all enemies
-
+dead_enemies = []
 
 class Enemy(Entity):
-    """Class for enemies, containing their"""
+    """Class for enemies, containing their position and direction of movement"""
     def __init__(self, initial_angle):
-        super().__init__(initial_angle, RADIUS)  # TODO: randomly determine an initial angle that'll not go out
-        self.direction = 0  # Direction enemy is moving
+        super().__init__(initial_angle, RADIUS)
+        self.direction = 0  # Direction enemy is moving  # TODO: randomly determine an initial angle that'll not go out
 
     def update(self, dt):
         pass
+
+    def __repr__(self):
+        return str(self)#f"Enemy({self.angle})"
+
+    def __str__(self):
+        return f"Enemy at ({self.distance}, {self.angle}°), moving in direction {self.direction}°"
+
+spawn_enemy = object_reuse(ENEMIES, dead_enemies, Enemy)[0]
+
+
 class Explosion(Entity):
     """Class for the explosions that appear when an enemy dies"""
     def __init__(self, angle, distance):
