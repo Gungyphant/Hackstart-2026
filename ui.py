9ffd787625e98312b24386f9d523424a4342f75c
jam1g26 <jam1g26@soton.ac.uk>
2026-09-19 12:18:47 +0100

    Updating entities and input handling on space

--- ui.py
+++ ui.py
@@ -1,25 +1,32 @@
 import pygame as py
-from globalvars import WIDTH, HEIGHT, RADIUS
-
-def draw(screen):
-    screen.fill((0,0,0))
+from globals import WIDTH, HEIGHT, RADIUS, ENTITIES, FPS
+from player import Player, Bullet, BULLETS
+from enemy import Enemy, ENEMIES
 
 py.init()
 screen = py.display.set_mode((WIDTH, HEIGHT))
 clock = py.time.Clock()
 running = True
 dt = 0
+turret = Player()
 
 while running:
 
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_SPACE:
+                turret.fire_bullet()
 
-        draw(screen)
+    for entity in ENTITIES:
+        entity.update(dt)
 
-        py.display.flip()
+    
 
-        dt = clock.tick(60)
+    screen.fill((0, 0, 0))
+
+    py.display.flip()
+    dt = clock.tick(FPS)
 
 
