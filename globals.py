8004f0f27bd53692ba2c7502f746c5455ddd3e9e
Gungyphant <gungyphant@gmail.com>
2026-09-19 11:53:43 +0100

    Added a list of all entities and added a docstring to Entity.update

--- globals.py
+++ globals.py
@@ -1,10 +1,15 @@
 WIDTH, HEIGHT = 1000,1000
 RADIUS = 450
 
+ENTITIES = []  # Global list of all entities
+
 class Entity:
     """Base class for all objects"""
     def __init__(self, initial_angle, initial_distance):
         self.angle = initial_angle
         self.distance = initial_distance
+        ENTITIES.append(self)
 
-    def update(self, dt): pass
+    def update(self, dt: float):
+        """Updates the entity to its new position after """
+        pass
