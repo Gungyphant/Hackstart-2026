38d52078b7ba93c3b28d6b6a49f134f3157cdb8c
Gungyphant <gungyphant@gmail.com>
2026-09-19 13:34:18 +0100

    Created Explosion

--- enemy.py
+++ enemy.py
@@ -11,3 +11,17 @@ class Enemy(Entity):
 
     def update(self, dt):
         pass
+class Explosion(Entity):
+    """Class for the explosions that appear when an enemy dies"""
+    def __init__(self, angle, distance):
+        super().__init__(angle, distance)
+        self.time_to_live = 500  # ms
+
+    def update(self, dt: float):
+        self.time_to_live -= dt
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"Explosion at ({self.distance}, {self.angle}°)"
