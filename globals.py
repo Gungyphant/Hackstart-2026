306d01020d3189d8e014aee1bdaf1b7c7903fe3c
Gungyphant <gungyphant@gmail.com>
2026-09-19 13:34:52 +0100

    Created object_reuse
    Also modified update's docstring

--- globals.py
+++ globals.py
@@ -12,5 +12,20 @@ class Entity:
         ENTITIES.append(self)
 
     def update(self, dt: float):
-        """Updates the entity to its new position after """
+        """Updates the entity to its new position after dt milliseconds have passed"""
         pass
+
+def object_reuse(active_list: list[object], inactive_list: list[object], object: object):
+    def create(*args, **kwargs):
+        if inactive_list:
+            new_object = inactive_list.pop()
+            new_object.__init__(*args, **kwargs)
+        else:
+            new_object = object(*args, **kwargs)
+        active_list.append(new_object)
+
+    def remove(self):
+        active_list.remove(self)
+        inactive_list.append(self)
+
+    return create, remove
