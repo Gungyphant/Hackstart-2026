dc3acdbed837c8ce8edb7a94a127bf3b738d28fa
Gungyphant <gungyphant@gmail.com>
2026-09-19 14:13:41 +0100

    Implemented enemy movement

--- enemy.py
+++ enemy.py
@@ -7,10 +7,11 @@ class Enemy(Entity):
     """Class for enemies, containing their position and direction of movement"""
     def __init__(self, initial_angle):
         super().__init__(initial_angle, RADIUS)
-        self.direction = 0  # Direction enemy is moving  # TODO: randomly determine an initial angle that'll not go out
+        self.initial_angle = initial_angle
 
     def update(self, dt):
-        pass
+        self.angle -= dt
+        self.distance = 100 * (self.angle + 4.5 - self.initial_angle)
 
     def __repr__(self):
         return str(self)#f"Enemy({self.angle})"
@@ -35,3 +36,13 @@ class Explosion(Entity):
 
     def __str__(self):
         return f"Explosion at ({self.distance}, {self.angle}°)"
+
+
+if __name__ == "__main__":
+    import time
+    last_time = time.time()
+    enemy = Enemy(1)
+    while True:
+        print(enemy)
+        enemy.update(time.time() - last_time)
+        last_time = time.time()
